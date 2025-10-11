3c8e613eb0f3f3cfcf2af52a9462157a90a8d3c5
devbrianmaina <brian.maina2@student.moringaschool.com>
2025-10-11 15:27:18 +0300

    Initial commit

---
+++ Server.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.net.*;
+import java.io.*;
+
+class Server{
+    public static void main(String[] args)
+    {
+        String data = "Toobie ornaught toobie. Our REG 20/00510.\n";
+        try{
+            ServerSocket srvr = new ServerSocket(1234);
+            Socket skt = srvr.accept();
+            System.out.print("Dear Brian Server has connected!!!\n");
+            PrintWriter out = new
+            PrintWriter(skt.getOutputStream(), true);
+            System.out.print("Sending string:" + data + "'\n");
+            out.print(data);
+            out.close();
+            skt.close();
+            srvr.close();
+        }
+        catch(Exception e ){ System.out.print("Whoops! it didnt work! \n");
+    }
+
+        }
+    }
