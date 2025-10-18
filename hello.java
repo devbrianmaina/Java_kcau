6796e6df82b0c754637fa96d32081865620d49a2
devbrianmaina <brian.maina2@student.moringaschool.com>
2025-10-18 12:13:53 +0300

    Ignore file testing

---
+++ hello.java
@@ -0,0 +1,190 @@
+import java.util.Scanner;
+
+class hello{
+    /**
+     * @param args
+     
+    static int age;
+    */
+    public static void main(String[] args){
+
+        /**
+     * Hello brian world program
+     * 
+     * System.out.println("Hello, world brian.");
+
+        */
+/**
+ * I am 31 Years old today
+ * integer declaration and assignment
+ */
+        // int age;
+        // age = 31;
+        // System.out.println("I am " + age + " Years old today");
+    // int number1 = 5;
+    // double number2 = number1;
+    // System.out.println(number2);
+
+/**
+ * 
+ * Basic Arithmetic Operations
+ */
+    // double number1 = 20;
+    // number1 += 8;
+    // double number2 = 6;
+  
+    // System.out.println("The sum of " + number1 + " and " + number2 +" is " + (number1 + number2));
+    // System.out.println("The division of " + number1 + " divided " + number2 +" is " + (number1 / number2));
+    // System.out.println("The remainder of " + number1 + " divided " + number2 +" is " + (number1 % number2));
+    // System.out.println("The notequality of " + number1 + " compared " + number2 +" is " + (number1 != number2));
+    // System.out.println("The equality of " + number1 + " compared " + number2 +" is " + (number1 == number2));
+    // System.out.println("is greater " + number1 + " compared " + number2 +" is " + (number1 >= number2));
+    // System.out.println("is smaller " + number1 + " compared " + number2 +" is " + (number1 <= number2));
+
+    // boolean isStudent = false;
+    // boolean isLibraryMember = false;
+    // System.out.println(!isStudent || isLibraryMember);
+    // System.out.println(isStudent || isLibraryMember);
+/**
+ * Simple Score and Turn Counter
+ */
+    // int score = 0;
+    // int turns = 10;
+
+    // score++;
+    // turns--;
+    // System.out.println("Score: " + score + " Turns: " + turns);
+    // System.out.println("Score increase is " + ++score);
+
+    /**
+     * String Comparison
+     */
+    // String name = "Brian is my name";
+    // String literalString1 = "abc";
+    // String literalString2 = "abc";
+
+
+    // System.out.println(name.length() + " " + name);
+
+    // String objectString1 = new String("xyz");
+    // String objectString2 = new String("xyc");
+
+    // System.out.println(literalString1 == literalString2); // true
+    // System.out.println(objectString1 == objectString2);   // false
+    
+
+    /**
+     * String Formatting Examples
+     */
+    // String name = "brothers"; // %S
+    // String  language = "java"; // %s
+    // double gpa = 3.85; // %f
+    // char precentsign = '%'; // %c
+    // boolean ofTruth = false;    // %b
+
+    // System.out.println(" Hello " + name + " , I am learning "+ language +" programming language");
+    // System.out.printf(" Hello %s I am learning %s programming language my gpa is %f. I have attempted 100%c of my courses. These are all %b claims", name, language, gpa, precentsign, ofTruth);
+
+    /**
+     * String Methods Examples
+     */
+    // String name = "Java programmer";
+    // String name2 = "Java Programmer";
+    // System.out.println(name.length());
+    // System.out.println(name.isEmpty());
+    // System.out.println(name.toUpperCase());
+    // System.out.println(name.toLowerCase());
+    // System.out.println(name.replace("Java", "king"));
+    // System.out.println(name.equals(name2));
+    // System.out.println(name.equalsIgnoreCase(name2));
+ /**
+  * user input example
+  */
+    // Scanner scanner = new Scanner(System.in);
+
+    // System.out.print("What is your name? ");
+    // String name = scanner.nextLine();
+    // System.out.printf("His name is %s", name.toLowerCase());
+    // System.out.println("How old are you?  ");
+    // int age = Integer.parseInt(scanner.nextLine());
+    // System.out.printf("You are %d yrs old", age);
+
+    // System.out.println("What lannguage do you prefer?");
+    // String language = scanner.nextLine();
+
+    // System.out.println("You prefer " + language + " programming language. ");
+
+    // scanner.close();
+
+    /**
+     * simple calculator
+     */
+//     Scanner scanner = new Scanner(System.in);
+
+//    System.out.println("Enter the first number: "); //first Number entry
+//    double num1 = Double.parseDouble(scanner.nextLine());
+//    System.out.println("Enter the second number: ");
+//    double num2 = Double.parseDouble(scanner.nextLine());
+
+//    System.out.println("What operation would you like to perform? (+, -, *, /): ");
+//    String operation = scanner.nextLine();
+//    switch (operation) {
+//     case "+":
+//         System.out.println("The sum is " + (num1 + num2));
+//         break;
+//     case "-":
+//         System.out.println("The subtraction is " + (num1 - num2));
+//         break;
+//     case "*":
+//         System.out.println("The product is " + (num1 * num2));
+//         break;
+//     case "/":
+//         if (num2 != 0){
+//             System.out.println("The quotient is " + (num1 / num2));
+//         } else {
+//             System.out.println("Error: Division by zero is not allowed.");
+//         }
+//         break;
+   
+//     default:
+//     System.out.println("invalid operation entry");
+//         break;
+//    }
+
+//    if (operation.equals("+")){
+//         System.out.println("The sum is " + (num1 + num2));
+//    }
+//     else if (operation.equals("-")){
+//         System.out.println("The difference is " + (num1 - num2));
+//     }
+//     else if (operation.equals("*")){
+//         System.out.println("The product is " + (num1 * num2));
+//     }
+//     else if (operation.equals("/")){
+//         if (num2 != 0){
+//             System.out.println("The quotient is " + (num1 / num2));
+//         } else {
+//             System.out.println("Error: Division by zero is not allowed.");
+//         }
+//     } else {
+//         System.out.println("Invalid operation selected.");
+//    }
+
+
+
+    // scanner.close();
+  
+
+
+
+    
+
+    
+
+
+
+
+
+
+    }
+}
